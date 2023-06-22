6726127ef2a697cb1d91796fd7d6dfd3602f5974
Dimitri Steinel <dimitri.steinel@bsdex.de>
2023-06-22 11:02:40 +0200

    Init

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,16 +1,14 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import { mockIonicReact } from "@ionic/react-test-utils"
+import "@testing-library/jest-dom/extend-expect"
+mockIonicReact()
 
 // Mock matchmedia
-window.matchMedia = window.matchMedia || function() {
-  return {
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
       matches: false,
-      addListener: function() {},
-      removeListener: function() {}
-  };
-};
+      addListener: function () {},
+      removeListener: function () {},
+    }
+  }
